db100860a361d2a4af0c6596ddff9ad3cc36ebd2
Makito <i@keep.moe>
2022-01-22 13:30:14 +0800

    docs: add comments

--- signal.go
+++ signal.go
@@ -6,6 +6,8 @@ import (
 	"syscall"
 )
 
+// terminalSignalCh returns a channel that waits for signals which usually indicates
+// the terminal of a process.
 func terminalSignalCh() <-chan os.Signal {
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
