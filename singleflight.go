c9e004800dda823f0503e6645534a5b3ddb773dc
Makito <i@keep.moe>
2022-01-22 13:23:12 +0800

    fix: close channel in singleflight

--- singleflight.go
+++ singleflight.go
@@ -2,6 +2,8 @@ package raft
 
 import "sync/atomic"
 
+// SingleFlight executes the function only once on the first Do() call,
+// returns the function return value and save it for future Do() calls.
 type SingleFlight struct {
 	c atomic.Value
 	v interface{}
@@ -11,9 +13,9 @@ func (s *SingleFlight) Do(f func() interface{}) interface{} {
 	c := make(chan struct{}, 1)
 	if s.c.CompareAndSwap(nil, c) {
 		s.v = f()
-		close(c)
 	} else {
 		<-s.c.Load().(chan struct{})
 	}
+	close(c)
 	return s.v
 }
