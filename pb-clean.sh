7dec63b9f78ad424628a2acc9f93503425a90730
Makito <i@keep.moe>
2022-01-25 17:41:34 +0800

    style: remove newlines

--- pb-clean.sh
+++ pb-clean.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
 find . -iname "*.pb.go" -type f -delete
--- pb-gen.sh
+++ pb-gen.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 build_protos() {
     protoc --proto_path=$1 --go_out=$1 --go_opt=paths=source_relative \
         --go-grpc_out=$1 --go-grpc_opt=paths=source_relative \
         $(find $1 -iname "*.proto")
 }
-
 build_protos pb
