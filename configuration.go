5bffd03569c00d3fcb0e19a8652ef354ae5dcebf
Makito <i@keep.moe>
2022-02-09 14:39:35 +0800

    refactor: rename and add committed configuration

--- configuration.go
+++ configuration.go
@@ -7,21 +7,21 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-type Config struct {
+type config struct {
 	peerMap SingleFlight[map[string]*pb.Peer]
 
 	*pb.Config
 }
 
-func newConfig(pbConfig *pb.Config) *Config {
-	return &Config{Config: pbConfig.Copy()}
+func newConfig(pbConfig *pb.Config) *config {
+	return &config{Config: pbConfig.Copy()}
 }
 
-func (c *Config) Copy() *Config {
+func (c *config) Copy() *config {
 	return newConfig(c.Config.Copy())
 }
 
-func (c *Config) Contains(serverId string) bool {
+func (c *config) Contains(serverId string) bool {
 	peerMap := c.peerMap.Do(func() map[string]*pb.Peer {
 		m := map[string]*pb.Peer{}
 		for _, p := range c.Peers {
@@ -33,32 +33,38 @@ func (c *Config) Contains(serverId string) bool {
 	return ok
 }
 
-func (c *Config) Quorum() int {
+func (c *config) Quorum() int {
 	return len(c.Peers)/2 + 1
 }
 
-type Configuration struct {
-	logIndex uint64
+type configuration struct {
+	*pb.Configuration
 
 	peerMapSingle      SingleFlight[map[string]*pb.Peer]
 	peersSingle        SingleFlight[[]*pb.Peer]
 	currentPeersSingle SingleFlight[[]*pb.Peer]
 
-	currentSingle SingleFlight[*Config]
-	nextSingle    SingleFlight[*Config]
+	currentSingle SingleFlight[*config]
+	nextSingle    SingleFlight[*config]
 
-	*pb.Configuration
+	logIndex uint64
 }
 
-func newConfiguration(pbConfiguration *pb.Configuration) *Configuration {
-	return &Configuration{Configuration: pbConfiguration}
+var nilConfiguration = newConfiguration(&pb.Configuration{Current: &pb.Config{Peers: []*pb.Peer{}}}, 0)
+
+func newConfiguration(pbConfiguration *pb.Configuration, logIndex uint64) *configuration {
+	return &configuration{Configuration: pbConfiguration, logIndex: logIndex}
 }
 
-func (c *Configuration) Copy() *Configuration {
-	return newConfiguration(c.Configuration.Copy())
+func (c *configuration) Copy(logIndex uint64) *configuration {
+	return newConfiguration(c.Configuration.Copy(), logIndex)
 }
 
-func (c *Configuration) peerMap() map[string]*pb.Peer {
+func (c *configuration) LogIndex() uint64 {
+	return c.logIndex
+}
+
+func (c *configuration) peerMap() map[string]*pb.Peer {
 	return c.peerMapSingle.Do(func() map[string]*pb.Peer {
 		m := map[string]*pb.Peer{}
 		for i := range c.Current.Peers {
@@ -79,7 +85,7 @@ func (c *Configuration) peerMap() map[string]*pb.Peer {
 	})
 }
 
-func (c *Configuration) peers() []*pb.Peer {
+func (c *configuration) peers() []*pb.Peer {
 	if c.Next == nil || len(c.Next.Peers) == 0 {
 		return c.currentPeersSingle.Do(func() []*pb.Peer {
 			peers := make([]*pb.Peer, 0, len(c.Current.Peers))
@@ -100,43 +106,41 @@ func (c *Configuration) peers() []*pb.Peer {
 	})
 }
 
-func (c *Configuration) CurrentConfig() *Config {
-	return c.currentSingle.Do(func() *Config { return newConfig(c.Current) })
+func (c *configuration) CurrentConfig() *config {
+	return c.currentSingle.Do(func() *config { return newConfig(c.Current) })
 }
 
-func (c *Configuration) NextConfig() *Config {
+func (c *configuration) NextConfig() *config {
 	if c.Next == nil {
 		return nil
 	}
-	return c.nextSingle.Do(func() *Config { return newConfig(c.Next) })
+	return c.nextSingle.Do(func() *config { return newConfig(c.Next) })
 }
 
-func (c *Configuration) Joint() bool {
+func (c *configuration) Joint() bool {
 	return c.Next != nil
 }
 
-func (c *Configuration) Peer(serverId string) *pb.Peer {
+func (c *configuration) Peer(serverId string) *pb.Peer {
 	if p, ok := c.peerMap()[serverId]; ok {
 		return p
 	}
 	return nil
 }
 
-func (c *Configuration) Peers() []*pb.Peer {
+func (c *configuration) Peers() []*pb.Peer {
 	return c.peers()
 }
 
-var nilConfiguration = newConfiguration(
-	&pb.Configuration{Current: &pb.Config{Peers: []*pb.Peer{}}},
-)
-
 type configurationStore struct {
-	server *Server
-	latest atomic.Value // *Configuration
+	server    *Server
+	committed atomic.Value // *Configuration
+	latest    atomic.Value // *Configuration
 }
 
 func newConfigurationStore(server *Server) (*configurationStore, error) {
 	c := &configurationStore{server: server}
+	c.committed.Store(nilConfiguration)
 	c.latest.Store(nilConfiguration)
 
 	// Find the latest configuration
@@ -149,7 +153,7 @@ func newConfigurationStore(server *Server) (*configurationStore, error) {
 		if err := proto.Unmarshal(log.Body.Data, &conf); err != nil {
 			return nil, err
 		}
-		c.latest.Store(&Configuration{logIndex: log.Meta.Index, Configuration: &conf})
+		c.latest.Store(newConfiguration(&conf, log.Meta.Index))
 	}
 
 	return c, nil
@@ -160,8 +164,8 @@ func newConfigurationStore(server *Server) (*configurationStore, error) {
 // Should not be called when the server is already in a joint consensus.
 // When the leader prepares to change the configuration, this should be the only
 // function to call.
-func (s *configurationStore) InitiateTransition(next *Config) error {
-	latest := s.latest.Load().(*Configuration)
+func (s *configurationStore) InitiateTransition(next *config) error {
+	latest := s.latest.Load().(*configuration)
 	if latest.Joint() {
 		return ErrInJointConsensus
 	}
@@ -184,7 +188,7 @@ func (s *configurationStore) InitiateTransition(next *Config) error {
 // configuration for joint consensus and appends the configuration log.
 // Should not be called when the server is not in a joint consensus.
 func (s *configurationStore) CommitTransition() error {
-	latest := s.latest.Load().(*Configuration)
+	latest := s.latest.Load().(*configuration)
 	if !latest.Joint() {
 		return ErrNotInJointConsensus
 	}
@@ -203,11 +207,26 @@ func (s *configurationStore) CommitTransition() error {
 	return nil
 }
 
-func (s *configurationStore) Latest() *Configuration {
-	return s.latest.Load().(*Configuration)
+func (s *configurationStore) Joint() bool {
+	return s.latest.Load().(*configuration).Joint()
 }
 
-func (s *configurationStore) SetLatest(c *Configuration) {
+func (s *configurationStore) Committed() *configuration {
+	return s.committed.Load().(*configuration)
+}
+
+func (s *configurationStore) SetCommitted(c *configuration) {
+	if c == nil {
+		c = nilConfiguration
+	}
+	s.committed.Store(c)
+}
+
+func (s *configurationStore) Latest() *configuration {
+	return s.latest.Load().(*configuration)
+}
+
+func (s *configurationStore) SetLatest(c *configuration) {
 	if c == nil {
 		c = nilConfiguration
 	}
